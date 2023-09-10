2083b6de0c05031c3c12b05fff4d948e3c7003e8
Saksham Sangal <sakshamsangal111@gmail.com>
2023-09-10 11:46:42 +0530

    up

--- src/com/app/Solution.java
+++ src/com/app/Solution.java
@@ -1,7 +1,58 @@
 package com.app;
 
 public class Solution {
+
     public static void main(String[] args) {
-        System.out.println(" = ");
+        Main main = new Main();
+        int[] arr = {3, 1};
+        int search = main.search(arr, 1);
+        System.out.println("search = " + search);
     }
+}
+
+class Main {
+    public int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == target) return mid;
+
+            // chances in which range target belongs to
+            if (arr[start] < arr[mid]) {
+
+                // 1st part is sorted
+                // if target is in this range
+                if (arr[start] <= target && target <= arr[mid]) {
+
+                    // change end
+                    end = mid - 1;
+                }
+
+                // may be target is in 2nd range
+                else {
+                    start = mid + 1;
+                }
+            }
+
+            // 2nd part is sorted
+            else {
+
+                // check if target lies in this range
+                if (arr[mid] <= target && target <= arr[end]) {
+                    start = mid + 1;
+                }
+
+                // target not lie in sorted range
+                else {
+                    end = mid - 1;
+                }
+            }
+
+        }
+        return -1;
+    }
+
+
 }
--- src/com/app/search/Search.java
+++ src/com/app/search/Search.java
@@ -3,6 +3,50 @@ package com.app.search;
 public class Search {
     static int count = 1;
 
+    public int searchRotatedArray(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == target) return mid;
+
+            // chances in which range target belongs to
+            if (arr[start] < arr[mid]) {
+
+                // 1st part is sorted
+                // if target is in this range
+                if (arr[start] <= target && target <= arr[mid]) {
+
+                    // change end
+                    end = mid - 1;
+                }
+
+                // may be target is in 2nd range
+                else {
+                    start = mid + 1;
+                }
+            }
+
+            // 2nd part is sorted
+            else {
+
+                // check if target lies in this range
+                if (arr[mid] <= target && target <= arr[end]) {
+                    start = mid + 1;
+                }
+
+                // target not lie in sorted range
+                else {
+                    end = mid - 1;
+                }
+            }
+
+        }
+        return -1;
+    }
+
+
     // normal binary search program
     static boolean binarySearch(int[] arr, int item) {
         int start = 0;
